7edce552f426c942e48d79d241d469281ffc435c
huangweizhong <yo577306@gmail.com>
2023-10-04 14:54:00 +0800

    feat:todolist_CRUD

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'created_at',
+        'updated_at',
+        'name',
+        'description',
+        // 白名單
+        // 複寫Laravel原先create的設定
+        // 這些欄位可以被批量寫入
+    ];
+    
+    protected $guarded = [
+        // 黑名單
+        // 複寫Laravel原先create的設定
+        // 除這些欄位外，其他欄位皆可以被批量寫入
+    ];
 }
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,49 @@ use App\Http\Controllers\TodoController;
 |
 */
 
+Route::get('/hello/{name?}', function ($name="World"){
+    return view("hello-name", [
+        "name" => $name,
+    ]);
+});
+
+Route::get('/number/{number}', function ($number){
+    return $number;
+})->where(["number"=>'[0-9]+']); // 設定條件：輸入的內容要由0-9組成
+
+Route::any('/hello-world/', function (){
+    return '<h1>Hello, World</h1>';
+})->name("hello-world"); // 將路由命名為hello-world
+
+// 將url分群好做分類管理，以下兩種寫法：
+// Route::prefix('admin')->group(function (){
+//     Route::get('users', function (){
+//         // Matches The "/admin/users" URL
+//     });
+// });
+
+Route::group(['prefix'=>'admin'], function(){
+    Route::get('users', function(){
+        // Match The "admin/users" URL
+    });
+    // Route::get('/{activity_id}', '$callback');
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/todo', [TodoController::class, 'index']);
+Route::apiResource('todos', TodoController::class);
+
+Route::group(['prefix' => 'todo',
+              'as' => 'todo/',
+              'namespace' => 'Todo',],
+
+              function(){
+                Route::get('/', [TodoController::class, 'index']);
+                Route::post('create', [TodoController::class, 'create']);
+                Route::delete('delete/{id}', [TodoController::class, 'delete']);
+                Route::put('update/{id}', [TodoController::class, 'update']);
+                Route::post('search/{name}', [TodoController::class, 'search']);
+                Route::post('details/{id}', [TodoController::class, 'details']); 
+              });
